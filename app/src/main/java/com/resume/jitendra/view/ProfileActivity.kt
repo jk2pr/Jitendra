917ebf3ab92e660da0187ec3e6b154f327be69ec
jitendra <jk2praj@gmail.com>
2018-02-16 19:09:20 +0530

    add dependency for viewModel

--- app/src/main/java/com/resume/jitendra/view/ProfileActivity.kt
+++ app/src/main/java/com/resume/jitendra/view/ProfileActivity.kt
@@ -1,17 +1,25 @@
-package com.resume.jitendra
+package com.resume.jitendra.view
 
+import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-import kotlinx.android.synthetic.main.activity_profile.*
+import com.resume.jitendra.R
+import com.resume.jitendra.viewmodel.ProfileViewModel
+
 
 class ProfileActivity : AppCompatActivity() {
 
+    private lateinit var viewModel:ProfileViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        viewModel = ViewModelProviders.of(this).get(ProfileViewModel::class.java)
+       /* viewModel.getUser().observe(this, { user ->
+            // update UI
+        })*/
        // setSupportActionBar(toolbar)
 
     }
